4628c7cc35c4c371d59d05a2c9fbe6d23b83e5fc
Samuel Birky <sbirky21@gmail.com>
2019-08-26 11:07:14 -0500

    intro

---
+++ intro.py
@@ -0,0 +1,12 @@
+#list
+my_list = ["sam", "sydney", "joe", True, {"name":"brian"}]
+print(my_list)
+my_list.append("joe2")
+my_list.extend(["stuff", "stuff", "stuff"])
+new_list = my_list.count("stuff")
+my_list.insert(3, "stuff5")
+new_new_list = list("cool")
+print(new_new_list)
+new_new_list.pop(0)
+
+#dictionary set tuple - check notes
